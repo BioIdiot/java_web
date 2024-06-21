625a594b20a209efeca100aaa48943c9c66a9afd
junyi <2841765064@qq.com>
2024-06-21 16:14:35 +0800

    下发令牌

--- src/main/java/com/junyi/java_web/controller/loginController.java
+++ src/main/java/com/junyi/java_web/controller/loginController.java
@@ -2,8 +2,12 @@ package com.junyi.java_web.controller;
 
 import com.junyi.java_web.domain.ManagerInfo;
 import com.junyi.java_web.service.impl.LoginServiceImpl;
+import com.junyi.java_web.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <p>
@@ -25,7 +29,15 @@ public class loginController {
        System.out.println(managerInfo.getId());
        boolean flag = loginService.login(managerInfo.getId().toString(),
                                          managerInfo.getPassword());
-        return new Result(flag ? Code.LOGIN_OK:Code.LOGIN_ERR , flag);
+
+       if(flag){
+           Map<String, Object> claims = new HashMap<>();
+           claims.put("id",managerInfo.getId());
+           String jwt = JwtUtils.generataJWT(claims);
+           return new Result(Code.LOGIN_OK, jwt);
+       }
+
+        return new Result(Code.LOGIN_ERR, flag);
    }
 
 }
